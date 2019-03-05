d06936113cde6343e120d9be9852be7ba1791483
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2019-03-05 11:37:55 -0500

    Updates

---
+++ precache-manifest.ccd1c8f878880c4aae9544cdf3a86f93.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "ed4d924303c7b43e0699",
+    "url": "/clicky-game/static/js/main.ed4d9243.chunk.js"
+  },
+  {
+    "revision": "179a064784d3fb038987",
+    "url": "/clicky-game/static/js/2.179a0647.chunk.js"
+  },
+  {
+    "revision": "7a5190df267aacb01c076cda7e833acc",
+    "url": "/clicky-game/index.html"
+  }
+];
